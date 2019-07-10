d79261bfd8b20ee1dec5c16787e8a11a794c884d
Amir Perehodnik <43294802+sl4vkek@users.noreply.github.com>
2019-07-10 14:25:44 +0300

    Added dateparser to requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='socialblade',
-      version='1.0.0',
+      version='1.0.1',
       description='Object oriented SocialBlade API wrapper',
       long_description=open('README.rst', 'rb').read().decode(),
       author='sl4v',
       author_email='iamsl4v@protonmail.com',
       url='https://github.com/sl4vkek/python-socialblade',
       packages=['socialblade'],
-      install_requires=['requests'],
+      install_requires=['requests', 'dateparser'],
       license="WTFPL"
      )
