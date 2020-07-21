58a37b65790905edce6aab61465ad2d9b678bfd9
Amir Yosef Perehodnik <43294802+sl4vkek@users.noreply.github.com>
2020-07-21 14:18:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='socialblade',
-      version='1.0.3.3',
+      version='1.0.3.4',
       description='Object oriented SocialBlade API wrapper',
       long_description=open('README.rst', 'r').read() ,
       author='sl4v',
